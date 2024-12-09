8674de8e09463a20e45de660ebe5c1b9c31ec7a9
Ceylin <caltepeceylin@gmail.com>
2024-12-09 10:11:53 +0300

    feat: create pages

--- TodoApp/src/app/app.module.ts
+++ TodoApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { TodoAddComponent } from './pages/todo-add/todo-add.component';
+import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoAddComponent,
+    TodoUpdateComponent
   ],
   imports: [
     BrowserModule,
---
+++ TodoApp/src/app/pages/todo-update/todo-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-update',
+  standalone: false,
+  
+  templateUrl: './todo-update.component.html',
+  styleUrl: './todo-update.component.css'
+})
+export class TodoUpdateComponent {
+
+}
