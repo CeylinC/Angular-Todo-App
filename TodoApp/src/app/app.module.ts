1446e07df41886f62fa5b095f6b7329181d0a469
Ceylin <caltepeceylin@gmail.com>
2024-12-09 10:28:09 +0300

    feat: add models and service

--- TodoApp/src/app/app.module.ts
+++ TodoApp/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { TodoAddComponent } from './pages/todo-add/todo-add.component';
 import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment.prod';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { TodoUpdateComponent } from './pages/todo-update/todo-update.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: 'BASE_API_URL', useValue: environment.baseUrl}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
